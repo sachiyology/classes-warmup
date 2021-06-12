7b5ab19caf75b507ae4c542f33e6cfc2c416d66d
sachiyo sugaya <sachiyology@gmail.com>
2021-06-13 00:09:46 +0900

    first commit

---
+++ solutions.js
@@ -0,0 +1,25 @@
+class Cat {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const white = new Cat('hachi', 1);
+const black = new Cat('tama', 1);
+
+function neko() {
+console.log(Cat)
+}
+
+console.log(white);
+console.log(black);
+
+const catsArr = ['Cat1', 'Cats', 'Catd','Cat4','Cat5','Cat6']
+for (let i = 0; i < 6; i++){
+  console.log(catsArr);
+}
+
+// const catPerson = {
+//   cats : cats;
+// }
